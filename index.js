bd51414fd2eb3dd9656b27649f7ed3387b1087db
KurtisIvey <kurtiveycodes@gmail.com>
2024-03-25 10:42:57 -0500

    add create new contact

--- index.js
+++ index.js
@@ -18,7 +18,20 @@ conn.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, (err, userInfo) => {
   }
 });
 
-app.get("/", (req, res) => {
+// gen contact get all query
+app.get("/get", (req, res) => {
+  conn.query("Select Id, Name , Phone, Email FROM Contact", (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log("Total records" + result.totalSize);
+      res.json(result.records);
+    }
+  });
+});
+
+// creation of new contact in SF
+app.get("/create", (req, res) => {
   conn.sobject("Contact").create(
     {
       FirstName: "tester",
@@ -41,7 +54,6 @@ app.get("/", (req, res) => {
       res.json(result.records);
     }
   });
-  //res.send("Salesforce integration with nodejs");
 });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
